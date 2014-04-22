deb5f9d74e9a699abc017463bc7affaefb7c715a
Kohei Abe <ecothm.abe@gmail.com>
2014-04-22 22:35:34 +0900

    Initial Commit

---
+++ TouchRooter/BCOViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCOViewController.h
+//  TouchRooter
+//
+//  Created by 阿部耕平 on 2014/04/22.
+//  Copyright (c) 2014年 Kohei Abe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCOViewController : UIViewController
+
+@end
---
+++ TouchRooter/BCOViewController.m
@@ -0,0 +1,29 @@
+//
+//  BCOViewController.m
+//  TouchRooter
+//
+//  Created by 阿部耕平 on 2014/04/22.
+//  Copyright (c) 2014年 Kohei Abe. All rights reserved.
+//
+
+#import "BCOViewController.h"
+
+@interface BCOViewController ()
+
+@end
+
+@implementation BCOViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
